959c889cfa29c191742b4d167111dd0e3b90a6fc
Alex Ramensky <alexramensky@gmail.com>
2020-10-23 11:09:33 -0400

    Update INS-to-ROS messages

--- inertiallabs_ins/src/il_ins.cpp
+++ inertiallabs_ins/src/il_ins.cpp
@@ -8,12 +8,16 @@
 #include "ILDriver.h"
 
 //adding message type headers
+#include <inertiallabs_msgs/sensor_data.h>
 #include <inertiallabs_msgs/ins_data.h>
+#include <inertiallabs_msgs/gps_data.h>
+#include <inertiallabs_msgs/gnss_data.h>
+#include <inertiallabs_msgs/marine_data.h>
 
 //Publishers
 
 struct Context {
-	ros::Publisher publisher;
+	ros::Publisher publishers[5];
 	std::string imu_frame_id;
 };
 
@@ -23,57 +27,116 @@ void publish_device(IL::INSDataStruct *data, void* contextPtr)
 	static int seq=0;
 	seq++;
 
+	inertiallabs_msgs::sensor_data msg_sensor_data;
 	inertiallabs_msgs::ins_data msg_ins_data;
+	inertiallabs_msgs::gps_data msg_gps_data;
+	inertiallabs_msgs::gnss_data msg_gnss_data;
+	inertiallabs_msgs::marine_data msg_marine_data;
 
-	ros::Time timestamp=ros::Time::now();
-	
-	if(context->publisher.getNumSubscribers()>0)
+	ros::Time timestamp = ros::Time::now();
+
+	if (context->publishers[0].getNumSubscribers() > 0)
 	{
-		msg_ins_data.header.seq=seq;
-		msg_ins_data.header.stamp=timestamp;
-		msg_ins_data.header.frame_id=context->imu_frame_id;
-		msg_ins_data.GPS_INS_Time=data->GPS_INS_Time;
-		msg_ins_data.GPS_IMU_Time=data->GPS_IMU_Time;
-		msg_ins_data.Gyro.x=data->Gyro[0];
-		msg_ins_data.Gyro.y=data->Gyro[1];
-		msg_ins_data.Gyro.z=data->Gyro[2];
-		msg_ins_data.Acc.x=data->Acc[0];
-		msg_ins_data.Acc.y=data->Acc[1];
-		msg_ins_data.Acc.z=data->Acc[2];
-		msg_ins_data.OriQuat.w=data->Quat[0];
-		msg_ins_data.OriQuat.x=data->Quat[1];
-		msg_ins_data.OriQuat.y=data->Quat[2];
-		msg_ins_data.OriQuat.z=data->Quat[3];
-		msg_ins_data.INS_LLH.x=data->Latitude;
-		msg_ins_data.INS_LLH.y=data->Longitude;
-		msg_ins_data.INS_LLH.z=data->Altitude;
-		msg_ins_data.Vel_ENU.x=data->VelENU[0];
-		msg_ins_data.Vel_ENU.y=data->VelENU[1];
-		msg_ins_data.Vel_ENU.z=data->VelENU[2];
-		msg_ins_data.GNSS_LLH.x=data->LatGNSS;
-		msg_ins_data.GNSS_LLH.y=data->LonGNSS;
-		msg_ins_data.GNSS_LLH.z=data->AltGNSS;
-		msg_ins_data.GDOP = data->GDOP;
-		msg_ins_data.PDOP = data->PDOP;
-		msg_ins_data.HDOP = data->HDOP;
-		msg_ins_data.VDOP = data->VDOP;
-		msg_ins_data.TDOP = data->TDOP;
-		msg_ins_data.Satellites = data->SVsol;
-		msg_ins_data.GNSS_Info1 = data->GNSSInfo1;
-		msg_ins_data.GNSS_Info2 = data->GNSSInfo2;
-		msg_ins_data.Solution_Status = data->INSSolStatus;
-		msg_ins_data.GNSS_Heading_Type = data->AnglesType;
-		msg_ins_data.New_GNSS_Flags = data->NewGPS;
-		msg_ins_data.GNSS_Track_GND = data->Trk_gnd;
-		msg_ins_data.Diff_Age = data->DiffAge;
-		msg_ins_data.USW = data->USW;
-		context->publisher.publish(msg_ins_data);
+		msg_sensor_data.header.seq = seq;
+		msg_sensor_data.header.stamp = timestamp;
+		msg_sensor_data.header.frame_id = context->imu_frame_id;
+		msg_sensor_data.Mag.x = data->Mag[0];
+		msg_sensor_data.Mag.y = data->Mag[0];
+		msg_sensor_data.Mag.z = data->Mag[0];
+		msg_sensor_data.Accel.x = data->Acc[0];
+		msg_sensor_data.Accel.y = data->Acc[1];
+		msg_sensor_data.Accel.z = data->Acc[2];
+		msg_sensor_data.Gyro.x = data->Gyro[0];
+		msg_sensor_data.Gyro.y = data->Gyro[1];
+		msg_sensor_data.Gyro.z = data->Gyro[2];
+		msg_sensor_data.Temp = data->Temp;
+		msg_sensor_data.Vinp = data->VSup;
+		msg_sensor_data.Pressure = data->hBar;
+		msg_sensor_data.Barometric_Height = data->pBar;
+		context->publishers[0].publish(msg_sensor_data);
+	}
+
+	if (context->publishers[1].getNumSubscribers() > 0)
+	{
+		msg_ins_data.header.seq = seq;
+		msg_ins_data.header.stamp = timestamp;
+		msg_ins_data.header.frame_id = context->imu_frame_id;
+		msg_ins_data.YPR.x = data->Heading;
+		msg_ins_data.YPR.y = data->Pitch;
+		msg_ins_data.YPR.z = data->Roll;
+		msg_ins_data.OriQuat.w = data->Quat[0];
+		msg_ins_data.OriQuat.x = data->Quat[1];
+		msg_ins_data.OriQuat.y = data->Quat[2];
+		msg_ins_data.OriQuat.z = data->Quat[3];
+		msg_ins_data.LLH.x = data->Latitude;
+		msg_ins_data.LLH.y = data->Longitude;
+		msg_ins_data.LLH.z = data->Altitude;
+		msg_ins_data.Vel_ENU.x = data->VelENU[0];
+		msg_ins_data.Vel_ENU.y = data->VelENU[1];
+		msg_ins_data.Vel_ENU.z = data->VelENU[2];
+		msg_ins_data.GPS_INS_Time = data->GPS_INS_Time;
+		msg_ins_data.GPS_IMU_Time = data->GPS_IMU_Time;
+		msg_ins_data.GPS_mSOW.data = data->ms_gps;
+		msg_ins_data.Solution_Status.data = data->INSSolStatus;
+        msg_ins_data.USW = data->USW;
+		context->publishers[1].publish(msg_ins_data);
+	}
+
+	if (context->publishers[2].getNumSubscribers() > 0)
+	{
+		msg_gps_data.header.seq = seq;
+		msg_gps_data.header.stamp = timestamp;
+		msg_gps_data.header.frame_id = context->imu_frame_id;
+		msg_gps_data.LLH.x = data->LatGNSS;
+		msg_gps_data.LLH.y = data->LonGNSS;
+		msg_gps_data.LLH.z = data->AltGNSS;
+		msg_gps_data.HorSpeed = data->V_Hor;
+		msg_gps_data.SpeedDir = data->Trk_gnd;
+		msg_gps_data.VerSpeed = data->V_ver;
+		context->publishers[2].publish(msg_gps_data);
+	}
+
+	if (context->publishers[3].getNumSubscribers() > 0)
+	{
+		msg_gnss_data.header.seq = seq;
+		msg_gnss_data.header.stamp = timestamp;
+		msg_gnss_data.header.frame_id = context->imu_frame_id;
+		msg_gnss_data.GNSS_info_1 = data->GNSSInfo1;
+		msg_gnss_data.GNSS_info_2 = data->GNSSInfo2;
+		msg_gnss_data.Number_Sat = data->SVsol;
+		msg_gnss_data.GNSS_Velocity_Latency = data->GNSSVelLatency;
+		msg_gnss_data.GNSS_Angles_Position_Type = data->AnglesType;
+		msg_gnss_data.GNSS_Heading = data->Heading_GNSS;
+		msg_gnss_data.GNSS_Pitch = data->Pitch_GNSS;
+		msg_gnss_data.GNSS_GDOP = data->GDOP;
+		msg_gnss_data.GNSS_PDOP = data->PDOP;
+		msg_gnss_data.GNSS_HDOP = data->HDOP;
+		msg_gnss_data.GNSS_VDOP = data->VDOP;
+		msg_gnss_data.GNSS_TDOP = data->TDOP;
+		msg_gnss_data.New_GNSS_Flags = data->NewGPS;
+		msg_gnss_data.Diff_Age = data->DiffAge;
+		context->publishers[3].publish(msg_gnss_data);
+	}
+
+	if (context->publishers[4].getNumSubscribers() > 0)
+	{
+		msg_marine_data.header.seq = seq;
+		msg_marine_data.header.stamp = timestamp;
+		msg_marine_data.header.frame_id = context->imu_frame_id;
+		msg_marine_data.Heave = data->Heave;
+		msg_marine_data.Surge = data->Surge;
+		msg_marine_data.Sway = data->Sway;
+		msg_marine_data.Heave_velocity = data->Heave_velocity;
+		msg_marine_data.Surge_velocity = data->Surge_velocity;
+		msg_marine_data.Sway_velocity = data->Sway_velocity;
+		msg_marine_data.Significant_wave_height = data->significant_wave_height;
+		context->publishers[4].publish(msg_marine_data);
 	}
 }
 
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
-	ros::init(argc,argv,"il_ins");
+	ros::init(argc, argv, "il_ins");
 	ros::NodeHandle n;
 	ros::NodeHandle np("~");
 	ros::Rate r(100); // 100 hz
@@ -85,48 +148,51 @@ int main(int argc,char** argv)
 
 	//command line varibales
 
-	np.param<std::string>("ins_url",port,"serial:/dev/ttyUSB0:460800");
-	np.param<int>("ins_output_format",ins_output_format,0x95);
+	np.param<std::string>("ins_url", port, "serial:/dev/ttyUSB0:460800");
+	np.param<int>("ins_output_format", ins_output_format, 0x52);
 
 	//Initializing Publishers
-
-	context.publisher = np.advertise<inertiallabs_msgs::ins_data> ("/Inertial_Labs/ins_data",1);
+	context.publishers[0] = np.advertise<inertiallabs_msgs::sensor_data>("/Inertial_Labs/sensor_data", 1);
+	context.publishers[1] = np.advertise<inertiallabs_msgs::ins_data>("/Inertial_Labs/ins_data", 1);
+	context.publishers[2] = np.advertise<inertiallabs_msgs::gps_data>("/Inertial_Labs/gps_data", 1);
+	context.publishers[3] = np.advertise<inertiallabs_msgs::gnss_data>("/Inertial_Labs/gnss_data", 1);
+	context.publishers[4] = np.advertise<inertiallabs_msgs::marine_data>("/Inertial_Labs/marine_data", 1);
 
 
 	ROS_INFO("connecting to INS at URL %s\n",port.c_str());
 
-	auto il_err=ins.connect(port.c_str());
-	if(il_err!=0) 
+	auto il_err = ins.connect(port.c_str());
+	if (il_err != 0)
 	{
 		ROS_FATAL("Could not connect to the INS on this URL %s\n",
-       	        port.c_str()
+				  port.c_str()
 		);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if (ins.isStarted())
 	{
 		ins.stop();
 	}
 	auto devInfo = ins.getDeviceInfo();
 	auto devParams = ins.getDeviceParams();
-	std::string SN(reinterpret_cast<const char *>(devInfo.IDN),8);
+	std::string SN(reinterpret_cast<const char *>(devInfo.IDN), 8);
 	ROS_INFO("Found INS S/N %s\n", SN.c_str());
 	context.imu_frame_id = SN;
 	il_err = ins.start(ins_output_format);
-	if(il_err!=0) 
+	if (il_err != 0)
 	{
 		ROS_FATAL("Could not start the INS: %i\n", il_err);
 		ins.disconnect();
 		exit(EXIT_FAILURE);
 	}
 	ins.setCallback(&publish_device, &context);
- 	ROS_INFO("publishing at %d Hz\n",devParams.dataRate);
+	ROS_INFO("publishing at %d Hz\n", devParams.dataRate);
 	ROS_INFO("rostopic echo the topics to see the data");
 	ros::spin();
-	std::cout << "Stopping INS..." << std::flush;
+	std::cout << "Stopping INS... " << std::flush;
 	ins.stop();
-	std::cout << "Disconnecting..." << std::flush;
+	std::cout << "Disconnecting... " << std::flush;
 	ins.disconnect();
 	std::cout << "Done." << std::endl;
 	return 0;
--- inertiallabs_msgs/README.md
+++ inertiallabs_msgs/README.md
@@ -4,28 +4,79 @@
 
   ```
 std_msgs/Header             header
-float64                     GPS_INS_Time 
-float64                     GPS_IMU_Time 
-geometry_msgs/Vector3       Gyro
-geometry_msgs/Vector3       Acc
+float64                     GPS_INS_Time
+float64                     GPS_IMU_Time
+std_msgs/UInt32             GPS_mSOW
+geometry_msgs/Vector3       LLH
+geometry_msgs/Vector3       YPR
 geometry_msgs/Quaternion    OriQuat
-geometry_msgs/Vector3       INS_LLH
 geometry_msgs/Vector3       Vel_ENU
-geometry_msgs/Vector3       GNSS_LLH
-float64                     GDOP
-float64                     PDOP
-float64                     HDOP
-float64                     VDOP
-float64                     TDOP
-uint8                       Satellites
-uint8                       GNSS_Info1
-uint8                       GNSS_Info2
-uint8                       Solution_Status
-uint8                       GNSS_Heading_Type
-uint8                       New_GNSS_Flags
-float64                     GNSS_Track_GND
-float64                     Diff_Age
-uint16                      USW                       
+std_msgs/Int8               Solution_Status
+geometry_msgs/Vector3       Pos_STD
+float32                     Heading_STD
+uint16                      USW
 
   ```
 
+  gnss_data
+
+  ```
+std_msgs/Header          header
+int8                     GNSS_info_1
+int8                     GNSS_info_2
+int8                     Number_Sat
+float32                  GNSS_Velocity_Latency
+int8                     GNSS_Angles_Position_Type
+float32                  GNSS_Heading
+float32                  GNSS_Pitch
+float32                  GNSS_GDOP
+float32                  GNSS_PDOP
+float32                  GNSS_HDOP
+float32                  GNSS_VDOP
+float32                  GNSS_TDOP
+uint8                    New_GNSS_Flags
+float64                  Diff_Age
+
+  ```
+
+  gps_data
+
+  ```
+
+std_msgs/Header           header
+geometry_msgs/Vector3     LLH
+float32                   HorSpeed
+float32                   SpeedDir
+float32                   VerSpeed
+
+  ```
+
+  sensor_data
+
+  ```
+
+std_msgs/Header           header
+geometry_msgs/Vector3     Mag
+geometry_msgs/Vector3     Accel
+geometry_msgs/Vector3     Gyro
+float32                   Temp
+float32                   Vinp
+float32                   Pressure
+float32                   Barometric_Height
+
+  ```
+
+  marine_data
+
+  ```
+
+  std_msgs/Header           header
+float64                   Heave
+float64                   Surge
+float64                   Sway
+float32                   Heave_velocity
+float32                   Surge_velocity
+float32                   Sway_velocity
+float32                   Significant_wave_height
+
+  ```
